43b617e2cceaeb198f6d7122d802a5681a1704d0
JeevithaSekar1326 <159133001+JeevithaSekar1326@users.noreply.github.com>
2024-04-26 10:58:57 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const Container = () =>{
        React.createElement(`div`,null,`Let's rock and roll`)
        );
 }
-class ReactContainer extends React.Component{
-    render(){
-        return (
-            <div>
-              Hello! Welcome to Kalvium 
-              <div>This is babel</div>  
-            </div>
-        );
-}
-}
+class ReactContainer extends React.Component{ 
+  
+    
+      render(){
+          return React.createElement(`div`,null,`Hey Kalvians`,
+          React.createElement(`div`,null,`Let's rock and roll with classes`)
+          );
+      }
+    
+  }
 const container = document.getElementById('react-container');
-ReactDOM.render(<ReactContainer/>,container);
+//ReactDOM.render("Hello! Welcome to React",container);
+//ReactDOM.render(React.createElement(Container),container);
+
+ReactDOM.render(React.createElement(ReactContainer),container);
